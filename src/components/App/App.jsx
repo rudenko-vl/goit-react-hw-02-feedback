60d0a6407c0f9bacf247f30a1108fdede9f8ee0f
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2022-01-06 18:45:05 +0200

    test deploy

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import { Container, Text } from "./App.styled";
+function App() {
+  return (
+    <Container>
+      <Text>Hello</Text>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  background: yellowgreen;
+`;
+export const Text = styled.p`
+  font-size: 20px;
+  font-weight: 600;
+  color: blue;
+`;
